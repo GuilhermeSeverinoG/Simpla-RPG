95714d1cce6998f6cef55a461182c368045b0820
Guilherme Severino Galvão <guilhermeseverinog@gmail.com>
2024-03-12 08:08:13 -0300

    Adicionada a função de ao clicar no select mostrar o status da classe

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var actIniciar = document.getElementById("act-iniciar")
+    actIniciar.addEventListener("click", function(){
+        window.location.href = 'game.html'
+    })
+})
