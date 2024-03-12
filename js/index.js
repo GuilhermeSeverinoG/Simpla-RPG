392cb9b7cfea585380d187364ee271a9a177676a
Guilherme Severino Galvão <guilhermeseverinog@gmail.com>
2024-03-12 08:21:09 -0300

    Adicionada a função de, ao clicar no botão iniciar, direcionar os dados da classe para a página game.html

--- js/index.js
+++ js/index.js
@@ -1,6 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(){
+    var playerStart = ["",0,0,0,0]//classe,hp,mp,ataque,defesa
+    var playerHp = document.getElementById("player-hp")
+    var playerMp = document.getElementById("player-mp")
+    var playerAttack = document.getElementById("player-attack")
+    var playerArmor = document.getElementById("player-armor")
+    var selectClass = document.getElementById("select-class")
     var actIniciar = document.getElementById("act-iniciar")
-    actIniciar.addEventListener("click", function(){
-        window.location.href = 'game.html'
+    var playerImage = document.getElementById("player-image")
+    selectClass.addEventListener("change", function(){
+        playerImage.innerHTML = '<img src="img/'+selectClass.value+'.png">'
+        if(selectClass.value == "wizard"){
+            playerStart = ["Mago",60,100,20,10]
+        }else{
+
+        }
+        updateMenuStatus()
     })
+    actIniciar.addEventListener("click", function(){
+        const url = 'game.html?'+
+        'classe='+encodeURIComponent(playerStart[0])+'&'+
+        'playerHp='+encodeURIComponent(playerStart[1])+'&'+
+        'playerMp='+encodeURIComponent(playerStart[2])+'&'+
+        'playerAttack='+encodeURIComponent(playerStart[3])+'&'+
+        'playerArmor='+encodeURIComponent(playerStart[4])
+        window.location.href = url
+    })
+    function updateMenuStatus(){
+        playerHp.innerHTML = playerStart[1]
+        playerMp.innerHTML = playerStart[2]
+        playerAttack.innerHTML = playerStart[3]
+        playerArmor.innerHTML = playerStart[4]
+    }
 })
