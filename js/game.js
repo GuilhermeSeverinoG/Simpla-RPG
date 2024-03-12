0bc73b98620fc0b2db4e7410d289cd412b5a6bed
Guilherme Severino Galvão <guilhermeseverinog@gmail.com>
2024-03-12 10:48:03 -0300

    Adicionados à janela principal os campos de imagem, vida e texto do evento; adicionado à janela esquerda o campo de ouro do player

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
-var event = [["Você entrou em confronto com uma blue slime!", 50, 5, 2],
-            ["Você entrou em confronto com uma green slime!", 50, 5, 2],
-            ["Você entrou em confronto com uma flame skull!", 80, 10, 10],
-            ["É um mímico!", 120, 15, 20],
-            ["Você achou uma bolsa com ouro",0,0,10],
-            ["Um baú esta diante de você", 0, 0, 0],
-            ["Dentro do baú tinha 50 de ouro", 0, 0, 50]]
+var event = [["m1","blue-slime.png","Você entrou em confronto com uma blue slime!", 50, 5, 2],
+             ["m2","green-slime.png","Você entrou em confronto com uma green slime!", 50, 5, 2],
+             ["m3","flame-skull.png","Você entrou em confronto com uma flame skull!", 80, 10, 10],
+             ["m4","mimic.png","É um mímico!", 120, 15, 20],
+             ["e1","chest.png","Um baú esta diante de você", 0, 0, 0],
+             ["e2","","Você achou uma bolsa com ouro",0,0,10]]
+             
 document.addEventListener("DOMContentLoaded", function(){
     //acoes
     const actSair = document.getElementById("act-sair");
